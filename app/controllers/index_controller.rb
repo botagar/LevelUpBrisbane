af6ac481ebd1a5652bf6dd99535157b0950d9db4
Magdalena Cervantes Cassel <magdalena.cassel@gmail.com>
2015-08-21 21:22:54 +1000

    Test the name we expect to see on the index page

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,4 +2,5 @@ class IndexController < ApplicationController
 	def home
 		@name = "John"
 	end
+  attr_reader :name
 end
--- test/controllers/index_controller_test.rb
+++ test/controllers/index_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
+require_relative '../../app/controllers/index_controller.rb'
 
 class IndexControllerTest < ActionController::TestCase
-  test "the truth" do
-    assert true
+  test "the name" do
+    expected_name = "John"
+
+    controller = IndexController.new
+    controller.home
+    assert (controller.name == expected_name), "Expected #{expected_name} but got #{controller.name}"
   end
 end
