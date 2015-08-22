8ee15748c5e831157948ecc552e8fe17b494c086
Magdalena Cervantes Cassel <magdalena.cassel@gmail.com>
2015-08-22 16:44:29 +1000

    FIX BUILD

--- test/controllers/index_controller_test.rb
+++ test/controllers/index_controller_test.rb
@@ -3,7 +3,7 @@ require_relative '../../app/controllers/index_controller.rb'
 
 class IndexControllerTest < ActionController::TestCase
   test "the name" do
-    expected_name = "John"
+    expected_name = "Magdalena"
 
     controller = IndexController.new
     controller.home
